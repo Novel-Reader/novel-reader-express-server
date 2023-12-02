cb56b0a16044f9a9a7b0b9fc5c40d0852c3a0aae
Michael An <2331806369@qq.com>
2023-12-02 22:37:22 +0800

    1.5 add file fields(tag and size) change API

--- src/sql/mysql.sql
+++ src/sql/mysql.sql
@@ -10,21 +10,23 @@ CREATE TABLE `user` (
 
 CREATE TABLE `book` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
   `cover_photo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `brief` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `brief` varchar(300) DEFAULT NULL,
   `detail` longtext DEFAULT NULL,
-  `created_at` datetime(6) DEFAULT NULL,
-  `price` decimal(11, 2),
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `price` decimal(11,2) DEFAULT NULL,
+  `size` int NOT NULL,·
+  `tag` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `comment` (
   `id` int NOT NULL AUTO_INCREMENT,
   `book_id` int CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `detail` longtext DEFAULT NULL,
-  `created_at` datetime(6) DEFAULT NULL,
+  `created_at` datetime(6) DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
